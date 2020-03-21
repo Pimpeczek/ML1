e67bc3f4d38f54f4f30c7ce47d5d860b4ba7161a
Piotr Wątorski <piotr.watorski@gmail.com>
2020-03-21 18:44:12 +0100

    Moved gen.Alg to separate library

--- ML1/Program.cs
+++ ML1/Program.cs
@@ -5,181 +5,105 @@ using System.Drawing;
 using System.Threading;
 using System.Linq;
 using System.Diagnostics;
+using ML1_Lib;
 namespace ML1
 {
     class Program
     {
+        //Mut 0.002 - 0.006
+        //Crs 0.8 - 1.0
+        //Tur 0.2 - 0.4
+        //Pop 100 - 1000
+        
+        static int taskCount = 1;
+        static int populationCount = 1;
+        static double[] mutationBorders = { 0.001, 0.004 };
+        static double[] crossoverBorders = { 0.8, 1 };
+        static double[] tournamenBorders = { 0.2, 0.4 };
+        static int[] populationBorders = { 100, 1000 };
+        static int[] itemCountBorders = { 1000, 1000 };
+        static int[] sizeBorders = { 10000, 10000 };
+        static int[] weightBorders = { 10000, 10000 };
+        static int density = 10;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("[1] - nutation tests\n[2] - crossover tests\n[3] - tournament tests\n[3] - population tests");
-            ConsoleKey key = Console.ReadKey(true).Key;
-            if (key == ConsoleKey.D1)
-                Calculate(MakePopMut, "mut_graph.png", 0);
-            if (key == ConsoleKey.D2)
-                Calculate(MakePopCrs, "crs_graph.png", 1);
-            if (key == ConsoleKey.D3)
-                Calculate(MakePopTur, "tur_graph.png", 2);
-            if (key == ConsoleKey.D4)
-                Calculate(MakePopPop, "pop_graph.png", 3);
 
-            Console.WriteLine("\n  - DONE! -");
-            Console.ReadKey(true);
-        }
+            Task[] tasks = new Task[10];
 
-        static void Calculate(Func<int, Task, int, int, Population> popCreator, string name, int pos)
-        {
-            Stopwatch stopwatch = new Stopwatch();
-            Population[] population;
-            Task[] tasks;
-            int temp;
-            
-            int groupSize = 8;
-            int taskCount = 8;
-            int[] pointBestPrice = new int[taskCount];
-            int bestPrice;
-            int[] pointWorstPrice = new int[taskCount];
-            int worstPrice;
-            int generationCount = 100;
-            int[] dims = { 10, generationCount };
-            int[] tileSize = { 10, 10 };
-            int[] imageSize = { tileSize[0] * dims[0], tileSize[1] * dims[1] };
-            int maxAverageScore = 0;
-            int averageScore;
-            int populationsCount = groupSize * taskCount;
-            int updateTime = 0;
-            Random random = new Random(10);
-            population = new Population[populationsCount];
-            tasks = new Task[taskCount];
-            Bitmap bmp = new Bitmap(imageSize[0], imageSize[1]);
-            Graphics graph = Graphics.FromImage(bmp);
-            SolidBrush[] brushes = new SolidBrush[256];
-            SolidBrush[] badBrushes = new SolidBrush[256];
-            int[,] data1 = new int[dims[0], dims[1]];
-            for (int br = 0; br < 256; br++)
+            Random rng = new Random();
+            Population population;
+            for(int t = 0; t < taskCount; t++)
             {
-                brushes[br] = new SolidBrush(Color.FromArgb(br, br, br));
-                badBrushes[br] = new SolidBrush(Color.FromArgb(br, 0, 0));
+                //tasks[t] = new Task(1000, 10000, 10000);
+                tasks[t] = new Task("testTask.task");
             }
 
-            for (int i = 0; i < taskCount; i++)
+            int[] popRange = new int[density];
+            double[] turRange = new double[density];
+            double[] crsRange = new double[density];
+            double[] mutRange = new double[density];
+            int tPop = (populationBorders[1] - populationBorders[0]) / (density);
+            double tTur = (tournamenBorders[1] - tournamenBorders[0]) / (density);
+            double tCrs = (crossoverBorders[1] - crossoverBorders[0]) / (density);
+            double tMut = (mutationBorders[1] - mutationBorders[0]) / (density);
+
+            int tempScore;
+            int bestScore = int.MinValue;
+            int bestPop;
+            double bestTur;
+            double bestCrs;
+            double bestMut;
+
+            for (int i = 0; i < density; i++)
             {
-                tasks[i] = new Task(random.Next(1000, 2000), random.Next(1000, 2000), random.Next(1000, 2000));
-                maxAverageScore += tasks[i].BestPossibleScore;
+                popRange[i] = populationBorders[0] + tPop * i;
+                turRange[i] = tournamenBorders[0] + tTur * i;
+                crsRange[i] = crossoverBorders[0] + tCrs * i;
+                mutRange[i] = mutationBorders[0] + tMut * i;
+
             }
 
-            stopwatch.Restart();
-            for (int m = 0; m < dims[0]; m++)
+            int counter = 0;
+            for (int pop = 9; pop < density; pop++)
             {
-                for (int t = 0; t < taskCount; t++)
+                for (int tur = 0; tur < density; tur++)
                 {
-                    for (int g = 0; g < groupSize; g++)
+                    for (int crs = 0; crs < density; crs++)
                     {
-                        population[t * groupSize + g] = popCreator.Invoke(m, tasks[t], g, g * 13);
-                    }
-                }
-                for (int gr = 0; gr < dims[1]; gr++)
-                {
-                    averageScore = 0;
-                    for (int t = 0; t < taskCount; t++)
-                    {
-                        for (int g = 0; g < groupSize; g++)
+                        for (int mut = 0; mut < density; mut++)
                         {
-                            temp = population[t * groupSize + g].Tournament();
+                            for (int t = 0; t < taskCount; t++)
+                            {
+                                
+                                for (int p = 0; p < populationCount; p++)
+                                {
+                                    counter++;
+                                    population = new Population(tasks[t], popRange[pop], (int)(popRange[pop] * turRange[tur]), crsRange[crs], mutRange[mut], 0.2, p * 13 + 13);
+                                    for(int i = 999; i >=0; i--)
+                                    {
+                                        if((tempScore = population.CreateNewGeneration()) > bestScore)
+                                        {
+                                            bestScore = tempScore;
+                                            bestPop = popRange[pop];
+                                            bestTur = turRange[tur];
+                                            bestCrs = crsRange[crs];
+                                            bestMut = mutRange[mut];
+                                            Console.SetCursorPosition(0,1);
+                                            Console.WriteLine($"Score: {bestScore}     \n Pop: {bestPop}     \n Tur: {bestTur}  \n Crs: {bestCrs}  \n Mut: {bestMut}  ");
+                                        }
+                                    }
 
-                            averageScore += temp;
-                            if (pointBestPrice[t] < temp)
-                                pointBestPrice[t] = temp;
-                            if (pointWorstPrice[t] > temp)
-                                pointWorstPrice[t] = temp;
-
+                                }
+                                Console.SetCursorPosition(0, 0);
+                                Console.Write(counter);
+                            }
                         }
-
-                    }
-                    averageScore /= populationsCount;
-                    data1[m, gr] = averageScore;
-                    if (updateTime + 200 < stopwatch.ElapsedMilliseconds)
-                    {
-                        Console.WriteLine($"{name} : generation [{gr + 1}/{dims[1]}] | test no. [{m + 1}/{dims[0]}]");
-                        updateTime = (int)stopwatch.ElapsedMilliseconds;
-                    }
-
-                }
-
-            }
-            bestPrice = 0;
-            worstPrice = 0;
-            for(int i = 0; i < taskCount; i++)
-            {
-                bestPrice += pointBestPrice[i];
-                worstPrice += pointWorstPrice[i];
-            }
-            bestPrice /= taskCount;
-            for (int m = 0; m < dims[0]; m++)
-            {
-                for (int gr = 0; gr < dims[1]; gr++)
-                {
-                    if (data1[m, gr] < 0)
-                    {
-                        graph.FillRectangle(badBrushes[data1[m, gr] * 255 / (worstPrice + 1)], m * tileSize[0], gr * tileSize[1], tileSize[0], tileSize[1]);
-                    }
-                    else
-                    {
-                        graph.FillRectangle(brushes[data1[m, gr] * 255 / (bestPrice + 1)], m * tileSize[0], gr * tileSize[1], tileSize[0], tileSize[1]);
+                        
                     }
                 }
             }
-            graph.Save();
-            graph.Dispose();
-            bmp.Save(name);
-        }
-
-        static Population MakePopMut(int m, Task t, int g, int s)
-        {
-            return new Population(t, 100, (g + 19) * (g + 19) * 13)
-            {
-                CrossoverRate = 0.9,
-                MutationRate = (double)m / 5000,
-                TournamentSize = 24
-            };
-        }
-        static Population MakePopCrs(int m, Task t, int g, int s)
-        {
-            return new Population(t, 100, (g + 19) * (g + 19) * 13)
-            {
-                CrossoverRate = (double)m/100,
-                MutationRate = 0.002,
-                TournamentSize = 24
-            };
-        }
-        static Population MakePopTur(int m, Task t, int g, int s)
-        {
-            return new Population(t, 100, (g + 19) * (g + 19) * 13)
-            {
-                CrossoverRate = 0.9,
-                MutationRate = 0.002,
-                TournamentSize = m
-            };
-        }
-        static Population MakePopPop(int m, Task t, int g, int s)
-        {
-            return new Population(t, m * 5 + 5, (g + 19) * (g + 19) * 13)
-            {
-                CrossoverRate = 0.9,
-                MutationRate = 0.002,
-                TournamentSize = m + 1
-            };
-        }
-
-        static void Generate(int objectCount, int maxWeight, int maxSize, string fileName)
-        {
-            Task task = new Task(objectCount, maxSize, maxWeight);
-            task.Save(fileName);
+            Console.ReadLine();
         }
     }
-
-    
-
-    
-
-    
 }
